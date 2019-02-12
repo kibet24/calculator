515ba79c1c28a98fa0ff75131d06e4a3c0d8aad2
Kibet <quibetamos@gmail.com>
2019-02-12 16:57:09 +0300

    Use add,subtract,multiply,devide and modulo

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var add = function add( a ,b){
+return a+b;
+
+
+};
+var a = parseInt(prompt("Enter number"));
+var b = parseInt(prompt("Enter number"));
+var result = add(a,b);
+alert(add(a,b));
+
+var subtract = function (a,b){
+  return a-b
+};
+alert(subtract(a,b));
+
+var times= function (a,b){
+  return a*b
+};
+alert(times(a,b));
+
+var division = function (a,b){
+  return a/b
+};
+alert(division(a,b));
+
+var modulo= function modulo(a,b){
+  return a%b;
+}
+alert(modulo(a,b));
